f51de7f7819e3aa99cc6f8e8730c41a6520bce22
Zazmuz <rasmuswario@gmail.com>
2024-12-12 23:57:37 +0100

    solved goggi

---
+++ open/goggi/goggi.py
@@ -0,0 +1,8 @@
+a, op, b = input().split()
+a, b = int(a), int(b)
+if a < b:
+    print("<")
+elif a > b:
+    print(">")
+else:
+    print("Goggi svangur!")
---
+++ open/goggi/golf.py
@@ -0,0 +1,3 @@
+a,i,b=input().split()
+a,b=int(a),int(b)
+print("<"if a<b else">"if a>b else"Goggi svangur!")
